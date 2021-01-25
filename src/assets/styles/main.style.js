a8231cb5c69648c0d87eaecc5988582b60c92d06
nickdpz <ndpastranzamora@gmail.com>
2021-01-24 21:49:08 -0500

    Route chat

---
+++ src/assets/styles/main.style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 40,
+    marginHorizontal: 30,
+  },
+  containerInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  containerJustify: {
+    justifyContent: 'center',
+  },
+  my2: {
+    marginHorizontal: 10,
+  },
+  mx2: {
+    marginHorizontal: 10,
+  },
+});
--- src/screens/main.js
+++ src/screens/main.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Text, View, Pressable } from 'react-native';
-import stylesForm from '../assets/styles/form.style';
+import { View } from 'react-native';
+import styles from '../assets/styles/main.style';
 import { logout } from '../redux/actions/user';
+import { Avatar, Card, Text, Icon } from 'react-native-elements';
 class Main extends Component {
-  handleLogout = async () => {
-    this.props.logout();
+  handleNavigation = async () => {
+    this.props.navigation.navigate('Chat');
   };
   render() {
     return (
-      <View>
-        <Text>Hola {this.props.user.name}</Text>
-        <Pressable onPress={this.handleLogout}>
-          <Text style={stylesForm.buttonForm}>Logout</Text>
-        </Pressable>
+      <View style={styles.container}>
+        <Card>
+          <View style={styles.containerInfo}>
+            <Avatar
+              rounded
+              source={{
+                uri: 'https://randomuser.me/api/portraits/men/41.jpg',
+              }}
+              size="large"
+            />
+            <Text
+              h3
+              style={styles.mx2}
+            >{`Hello ${this.props.user.name} ${this.props.user.lastName}`}</Text>
+          </View>
+        </Card>
+        <View
+          style={[styles.my, styles.containerInfo, styles.containerJustify]}
+        >
+          <Text h5>Talk with limpo bot </Text>
+          <Icon
+            raised
+            onPress={this.handleNavigation}
+            name="envelope-square"
+            type="font-awesome"
+          />
+        </View>
       </View>
     );
   }
